6b05cf58eb0aeed18e5a2d6a06c6b7e7881b5d2b
tagor <tagortrabajo@gmail.com>
2017-07-18 17:20:02 +0200

    updates+ remove your stuff

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickChange(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
