cf2eb87b61fa7a616423d989b2cdcb5c9b30c51b
tagor <tagortrabajo@gmail.com>
2017-07-18 16:33:56 +0200

    Last Updates

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -1,26 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 
-public class Respawn : MonoBehaviour {
+public class Pipe : MonoBehaviour {
 
-   
+    public GameObject Location2;
+
 	// Use this for initialization
 	void Start () {
-        //Debug.Log("hey");
-    }
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log("hit");
         //var Currentpos = other.gameObject.transform.position;
-        var Resetpos = new Vector3(0, 0, 0);
+        var Resetpos = Location2.transform.position;
         other.gameObject.transform.position = Resetpos;
     }
 }
