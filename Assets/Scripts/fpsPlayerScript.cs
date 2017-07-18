56e2c5b6fe550a47db83d6d5b34d573d99614e11
masoud karimnia <masoud.karimnia@gmail.com>
2017-07-18 11:37:10 +0200

    initial commit

---
+++ Assets/Scripts/fpsPlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsPlayerScript : MonoBehaviour {
+
+
+    CharacterController control;
+
+    // Use this for initialization
+    void Start () {
+        control = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        control.SimpleMove(new Vector3(0, 0, Input.GetAxis("Vertical") * 2));
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController controller;
+    Canvas canvas;  
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        //Text[] inputs = GameObject.Find("Canvas").GetComponentsInChildren<Text>();
+        //inputs[0].text = "Text here";
+    }
+
+    void Update()
+    {
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    //void OnGUI()
+    //{
+    //    GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "This is a box");
+    //}
+}
