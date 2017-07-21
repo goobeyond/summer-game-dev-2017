33b3c48e88dc6f927d7464c1fdcb9fadfc34dff7
masoud karimnia <masoud.karimnia@gmail.com>
2017-07-21 15:27:21 +0200

    fix character rotations

---
+++ Assets/FixRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour {
+
+
+    Quaternion rotation;
+    Vector3 origPos;
+	// Use this for initialization
+	void Start () {
+        rotation = this.transform.rotation;
+        origPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        transform.rotation = rotation;
+        transform.position = new Vector3(transform.position.x, transform.position.y, origPos.z);
+    }
+}
